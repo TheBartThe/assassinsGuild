04e33fe629d9fc327935d0215695a154dd01e882
TheBartThe <vieran96@gmail.com>
2020-01-10 17:20:42 +0000

    modified dockerfiles

--- applicationMission/application/mission.py
+++ applicationMission/application/mission.py
@@ -8,5 +8,5 @@ def getMission(targetService, weaponService):
     target = targetList["target"]
     points = (weaponList["weaponPoints"] * targetList["targetPoints"])
     credits = "credit" if (points == 1) else "credits"
-    message = "Your target is " + target + ". You must use a " + weapon + ". This will earn you " + str(points) + " " + credits + " with The Guild. Good luck."
+    message = "Right, here is your mission. Your target is " + target + ". You must use a " + weapon + ". This will earn you " + str(points) + " " + credits + " with The Guild. Good luck. You have 48 hours. This message will now self destruct."
     return {"message": message}
